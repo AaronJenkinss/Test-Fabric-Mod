8f23187c7dd02a90d23f044699bfd51129d057dc
Aaron-Jenkins <aaronjenkins22442244@gmail.com>
2022-12-22 18:14:38 +0530

    trying to add an item.

--- src/main/java/com/pulsesapphire/testmod/TestMod.java
+++ src/main/java/com/pulsesapphire/testmod/TestMod.java
@@ -1,6 +1,10 @@
 package com.pulsesapphire.testmod;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -8,9 +12,10 @@ public class TestMod implements ModInitializer {
 	public static final String MOD_ID = "testmod";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
+	public static final Item item1 = new Item(new Item.Settings());
+
 	@Override
 	public void onInitialize() {
-		// Adding a comment.
-		System.out.println("test");
+		Registry.register(Registries.ITEM, new Identifier("testItem", "test_item"), item1);
 	}
 }
